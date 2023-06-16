446b852d8dc03baaba3211c9ac20e630afdf7b09
vishwas verma <114891824+Vishwas-verma5@users.noreply.github.com>
2023-06-16 15:21:04 +0530

    push2

---
+++ app/src/main/java/secura/driver/scuradriver/CheckBalance.java
@@ -0,0 +1,14 @@
+package secura.driver.scuradriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckBalance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_balance);
+    }
+}
---
+++ app/src/main/java/secura/driver/scuradriver/EarningHistory.java
@@ -0,0 +1,14 @@
+package secura.driver.scuradriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EarningHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earning_history);
+    }
+}
