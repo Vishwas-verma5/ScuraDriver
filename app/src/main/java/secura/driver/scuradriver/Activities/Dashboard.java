622c06e9c455ad672fd62d0e71c1ef5f1b5bcc80
Pushpendra <azadsharma343@gmail.com>
2023-06-16 15:48:36 +0530

    This is a commit

--- app/src/main/java/secura/driver/scuradriver/Activities/Dashboard.java
+++ app/src/main/java/secura/driver/scuradriver/Activities/Dashboard.java
@@ -3,6 +3,7 @@ package secura.driver.scuradriver.Activities;
 import static secura.driver.scuradriver.Common.toggleColorStatusBar;
 import static secura.driver.scuradriver.Common.transparentStatusBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -10,11 +11,16 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import secura.driver.scuradriver.Fragments.RuppeFragment;
+import secura.driver.scuradriver.HomeFragment;
 import secura.driver.scuradriver.R;
 import secura.driver.scuradriver.SignIn;
 import secura.driver.scuradriver.databinding.ActivityDeshBoardBinding;
@@ -23,6 +29,8 @@ public class Dashboard extends AppCompatActivity {
     ActivityDeshBoardBinding binding;
     ActionBarDrawerToggle toggle;
 
+    HomeFragment homeFragment;
+    RuppeFragment ruppeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,28 @@ public class Dashboard extends AppCompatActivity {
         binding.drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         View header = binding.navigationBar.getHeaderView(0);
+
+        homeFragment=new HomeFragment();
+        ruppeFragment=new RuppeFragment();
+
+        //default fragment
+        fragload(homeFragment);
+
+        binding.dashNevbuttons.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                if (id==R.id.nav_home)
+                {
+                    fragload(homeFragment);
+                }
+                else if (id==R.id.nav_ruppe)
+                {
+                    fragload(ruppeFragment);
+                }
+                return true;
+            }
+        });
 
         LinearLayout incentives = header.findViewById(R.id.incentivesBtn);
         LinearLayout rewards = header.findViewById(R.id.rewards);
